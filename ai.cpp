4fdbe5771761585d4a950510f5955895546205a9
吳宜珈 <c14031081@mail.ncku.edu.tw>
2018-03-28 14:01:55 +0800

    test 2

--- ai.cpp
+++ ai.cpp
@@ -2,27 +2,30 @@
 #include <cstring>
 #include <cstdlib>
 #include <ctime>
+#include <sstream>
 using namespace std;
 
 int main() {
-    cout << "1 2 3 4 5 6 7 8" << endl;
-    string str;
+    cout << "6 9 1 8 C 7 3 2" << endl;
+    string str,token;
     //cin >> str;
-    //string deck[4];
+    string deck[4];
+    stringstream ss;
     while(getline(cin,str)) {
         if(str=="END") break;
-        /*string[] a=str.split(" ");
-        if(a[0]=="DECK") {
-            for(int i=1; i<5; i++) {
-                deck[i-1]=a[i];
-            }
-		}*/
-	}
-    int x,y;
+        ss.clear();
+        ss.str(str);
+        ss >> token;
+        if(token=="DECK") {
+            ss>>deck[0]>>deck[1]>>deck[2]>>deck[3];
+        }
+    }
+    int x[4],y[4];
     srand((unsigned)time(NULL));
-    x=rand()%11+12;
-    y=rand()%14+4;
-    //cout << 1 << " " << deck[rand()%4] << " " << x << " " << y << endl;
-    cout << "1" << " " << "1" << " " << x << " " << y << endl;
-	cout << "0";
+    for(int i=0; i<4; ++i) {
+        x[i]=rand()%11+12;
+        y[i]=rand()%14+4;
+        cout << 1 << " " << deck[rand()%4] << " " << x[i] << " " << y[i] << endl;
+    }
+    cout << "0";
 }
