01628caa67e660eeabe5cb851d457fb1024a4675
吳宜珈 <c14031081@mail.ncku.edu.tw>
2018-03-28 11:30:35 +0800

    commit

--- ai.cpp
+++ ai.cpp
@@ -7,17 +7,17 @@ using namespace std;
 int main() {
     cout << "1 2 3 4 5 6 7 8" << endl;
     string str;
-    cin >> str;
-    /*string deck[4];
+    //cin >> str;
+    //string deck[4];
     while(getline(cin,str)) {
         if(str=="END") break;
-        string[] a=str.split(" ");
+        /*string[] a=str.split(" ");
         if(a[0]=="DECK") {
             for(int i=1; i<5; i++) {
                 deck[i-1]=a[i];
             }
-        }
-    }*/
+		}*/
+	}
     int x,y;
     srand((unsigned)time(NULL));
     x=rand()%11+12;
