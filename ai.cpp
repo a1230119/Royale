cf643638b0fada3726f5dcf71d033608dda65cfe
吳宜珈 <c14031081@mail.ncku.edu.tw>
2018-03-28 16:21:34 +0800

    test 8

--- ai.cpp
+++ ai.cpp
@@ -11,7 +11,7 @@ int main() {
         string str,token,temp;
         string deck[4];
         string enemy[5];
-        int enpos[10][2];
+        int enpos[10];
         stringstream ss,ss1;
         int num=0;//enemy number
         while(getline(cin,str)) {
@@ -22,29 +22,23 @@ int main() {
             if(token=="DECK") {
                 ss>>deck[0]>>deck[1]>>deck[2]>>deck[3];
             }
-            /*
             if(token=="ENEMY"){
-            	int p=0;
-            	for(int i=0;i<5;i++){
-            		ss>enemy[i];
-            	}
+            	ss>>enemy[0]>>enemy[1]>>enemy[2]>>enemy[3]>>enemy[4];
             	ss1.clear();
             	ss1.str(enemy[2]);
             	while(getline(ss1,temp,',')){//get enemy position
-            		enpos[num][p]=atoi(temp);
-            		p++;
+            		enpos[num]=atoi(temp.c_str());
             	}
             	num++;
-            } */
+			}
         }
-        int x,y,sumx,sumy;
-        /*for(int i=0;i<num;i++){
-        	sumx+=enpos[i][0];
-        	sumy+=enpos[i][1];
-        } */
-        //srand((unsigned)time(NULL));
+        int x,y,sumx;
+        for(int i=0;i<num;i++){
+        	sumx+=enpos[i];
+        } 
+        srand((unsigned)time(NULL));
         for(int i=0; i<4; i++) {
-            x=rand()%14+4;
+            x=sumx/num;
             y=rand()%11+12;
             //x=sumx/num;
             //y=sumy/num;
