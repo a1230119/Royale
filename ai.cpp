31e12effe2dab706a8e918d92782307d735bcf8f
吳宜珈 <c14031081@mail.ncku.edu.tw>
2018-03-28 15:47:25 +0800

    test 5

--- ai.cpp
+++ ai.cpp
@@ -6,11 +6,13 @@
 using namespace std;
 
 int main() {
-    cout << "6 9 1 8 C 7 3 2" << endl;
-    string str,token;
-    //cin >> str;
+    cout << "2 3 1 8 6 9 C 7" << endl;
+    string str,token,temp;
     string deck[4];
-    stringstream ss;
+    string enemy[5];
+    int enpos[10][2];
+    stringstream ss,ss1;
+    int num=0;//enemy number
     while(getline(cin,str)) {
         if(str=="END") break;
         ss.clear();
@@ -19,12 +21,32 @@ int main() {
         if(token=="DECK") {
             ss>>deck[0]>>deck[1]>>deck[2]>>deck[3];
         }
+        /*
+        if(token=="ENEMY"){
+        	int p=0;
+        	for(int i=0;i<5;i++){
+        		ss>enemy[i];
+        	}
+        	ss1.clear();
+        	ss1.str(enemy[2]);
+        	while(getline(ss1,temp,',')){//get enemy position
+        		enpos[num][p]=atoi(temp);
+        		p++;
+        	}
+        	num++;
+        } */
     }
-    int x,y;
-    srand((unsigned)time(NULL));
+    int x,y,sumx,sumy;
+    /*for(int i=0;i<num;i++){
+    	sumx+=enpos[i][0];
+    	sumy+=enpos[i][1];
+    } */
+    //srand((unsigned)time(NULL));
     for(int i=0; i<4; i++) {
         x=rand()%14+4;
         y=rand()%11+12;
+        //x=sumx/num;
+        //y=sumy/num;
         cout << 1 << " " << deck[i] << " " << x << " " << y << endl;
     }
     cout << "0";
