c9bb85f7d5406944153c546ff67d2eade09e256c
吳宜珈 <c14031081@mail.ncku.edu.tw>
2018-03-28 10:39:07 +0800

    random

--- ai.cpp
+++ ai.cpp
@@ -6,5 +6,16 @@
 int main(){
 	cout << "1 2 3 4 5 6 7 8" << endl;
 	string str;
-	cin >> str;
+	//cin >> str;
+	char deck[5];
+	while(getline(cin,str)){
+		if(str=="END") break;
+		string[] a=str.split(" ");
+	}
+	int x,y;
+	srand((unsigned)time(NULL));
+	x=rand()%11+12;
+	y=rand()%14+4;
+	cout << 1 << " " << deck[rand()%4+1] << " " << x << " " << y << endl;
+	cout << "0";
 }
