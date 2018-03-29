30a87fd8a2ff7b56848b00ba363f34152792f2bf
吳宜珈 <c14031081@mail.ncku.edu.tw>
2018-03-29 18:50:36 +0800

    test 15

--- ai.cpp
+++ ai.cpp
@@ -6,14 +6,15 @@
 using namespace std;
 
 int main() {
-    cout << "1 4 9 6 5 7 3 C" << endl;
+    cout << "1 4 9 6 C 5 8 7" << endl;
     while(1) {
-        string str,token,temp;
+        string str,token,temp,Mana,fd,others;
         string deck[4];
         string enemy[5];
         int enpos[100];
         stringstream ss,ss1;
-        int num=0;//enemy number
+        int num=0,mana;//enemy number
+		bool nosgram=true;
         while(getline(cin,str)) {
             if(str=="END") break;
             ss.clear();
@@ -31,6 +32,15 @@ int main() {
                 }
                 num++;
             }
+			if(token=="FRIEND"){
+				ss>>fd>>others;
+				if(fd=="8")
+					nosgram=false;
+			}
+            /*if(toke=="MANA") {
+                ss>>Mana;
+                mana=atoi(Mana.c_str());
+            }*/
         }
         int x,y,sumx=0,pos=0;
         if(num!=0) {
@@ -39,6 +49,15 @@ int main() {
             }
             pos=sumx/num;
         }
+		bool sgram=false;
+		for(int i=0;i<4;i++){
+			if(nosgram)
+				break;
+			else if(deck[i]=="8"){
+			sgram=true;
+			break;
+			}
+		}
         srand((unsigned)time(NULL));
         for(int i=0; i<4; i++) {
             if(pos>=4 && pos <=17)
@@ -46,9 +65,10 @@ int main() {
             else
                 x=rand()%14+4;
             y=rand()%11+12;
-            //x=sumx/num;
-            //y=sumy/num;
-            cout << 1 << " " << deck[rand()%4] << " " << x << " " << y << endl;
+			if(sgram)
+				cout << 1 << " " << 8 << " " << 10 << " " << 12 << endl;
+			else
+				cout << 1 << " " << deck[i] << " " << x << " " << y << endl;
         }
         cout << "0";
     }
